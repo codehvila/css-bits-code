97c4ad18f0a9019d510e682d26ff57692f8df922
Hugo Vila <code.hvila@gmail.com>
2022-01-17 19:10:33 +0100

    Positioning HTML Elements

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
 // index.js
+
+const sectionHandlers = document.querySelectorAll(
+  'input[name="sectionposition"]'
+);
+
+const parentHandlers = document.querySelectorAll(
+  'input[name="parentposition"]'
+);
+const childHandlers = document.querySelectorAll('input[name="childposition"]');
+
+const setSectionPosition = () => {
+  console.log("section clicked ...");
+  let selectedPosition = null;
+  sectionHandlers.forEach((position) => {
+    if (position.checked) {
+      selectedPosition = position.value;
+    }
+  });
+  console.log(selectedPosition);
+  document.documentElement.style.setProperty("--section", selectedPosition);
+};
+
+const setParentPosition = () => {
+  console.log("parent clicked ...");
+  let selectedPosition = null;
+  parentHandlers.forEach((position) => {
+    if (position.checked) {
+      selectedPosition = position.value;
+    }
+  });
+  console.log(selectedPosition);
+  document.documentElement.style.setProperty("--parent", selectedPosition);
+};
+
+const setChildPosition = () => {
+  console.log("child clicked ...");
+  let selectedPosition = null;
+  childHandlers.forEach((position) => {
+    if (position.checked) {
+      selectedPosition = position.value;
+    }
+  });
+  console.log(selectedPosition);
+  document.documentElement.style.setProperty("--child", selectedPosition);
+};
+
+const addEventListeners = (handlers, e, func) => {
+  handlers.forEach((handler) => {
+    handler.addEventListener(e, func);
+  });
+};
+
+// sectionHandlers.forEach((handler) => {
+//   handler.addEventListener("click", setSectionPosition);
+// });
+
+// parentHandlers.forEach((handler) => {
+//   handler.addEventListener("click", setParentPosition);
+// });
+
+// childHandlers.forEach((handler) => {
+//   handler.addEventListener("click", setChildPosition);
+// });
+
+addEventListeners(sectionHandlers, "click", setSectionPosition);
+
+addEventListeners(parentHandlers, "click", setParentPosition);
+
+addEventListeners(childHandlers, "click", setChildPosition);
